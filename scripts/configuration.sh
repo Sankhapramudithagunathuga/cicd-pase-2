b45a339c26bdd1c679f1bdc75916860b054ee9b7
Sankhag <sankhag@mitesp.com>
2024-06-24 22:34:48 +0530

    add appspec.yaml

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,6 +2,8 @@ sudo apt-get update -y && sudo apt-get install -y unzip
 
 aws s3 cp s3://test-pack-s3-123456/p8-deployments-carbonplace-3.3.0.zip .
 unzip p8-deployments-carbonplace-3.3.0.zip
-mv p8-deployments p8-deployments-3.3.0
 
 ln -s /home/ubuntu/p8/p8-deployments-carbonplace-3.3.0 p8-deployments
+mv p8-deployments p8-deployments-3.3.0
+
+
