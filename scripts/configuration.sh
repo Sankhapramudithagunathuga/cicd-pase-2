38ff6f2ec948287a8999ad567ecaabbc93d7442e
Sankhag <sankhag@mitesp.com>
2024-06-25 16:40:48 +0530

    add appspec.yaml

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,12 +2,13 @@ sudo apt-get update -y && sudo apt-get install -y unzip
 
 #unlink p8-deployments
 
-aws s3 cp s3://test-pack-s3-123456/p8-deployments-carbonplace-3.3.0.zip .
-unzip p8-deployments-carbonplace-3.3.0.zip
+ws s3 cp s3://test-pack-s3-123456/p8-deployments-carbonplace-4.0.0.tar.gz .
+
+tar -xvf p8-deployments-carbonplace-4.0.0.tar.gz
 
 
-mv p8-deployments-carbonplace-3.3.0 p8-deployments-3.3.0 ##need to change
-ln -s /home/ubuntu/p8/p8-deployments-carbonplace-3.3.0 p8-deployments-carbonplace-3.3.0
+mv p8-deployments p8-deployments-carbonplace-4.0.0 ##need to change
+ln -s /home/ubuntu/p8/p8-deployments-carbonplace-4.0.0 p8-deployments
 #export P8_IM_SCRIPT_PATH=p8/p8-deployments/scripts
 
 
