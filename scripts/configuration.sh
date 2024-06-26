b65b5f8fed8c3b13a7255edbb164d7fc284f1b81
Sankhag <sankhag@mitesp.com>
2024-06-26 09:54:28 +0530

    add appspec.yaml

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -12,7 +12,7 @@ cd /home/ubuntu/p8
 sudo ln -s /home/ubuntu/p8/p8-deployments-carbonplace-4.0.0 p8-deployments
 export P8_IM_SCRIPT_PATH=p8/p8-deployments/scripts
 
-cd ~/p8-deployments/profiles
+cd /p8-deployments/profiles
 ./configure.sh -pvp carbon -pip production -uip yaalaprod apply
 
 
